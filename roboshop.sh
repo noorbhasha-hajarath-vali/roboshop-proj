09faf0b0554c23ddc66171da001b41459960b28e
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-07-13 23:44:39 +0530

    commiting roboshop file

--- roboshop.sh
+++ roboshop.sh
@@ -2,10 +2,31 @@
 
 AMI_ID=ami-002192a70217ac181
 SG_ID=sg-014ee579326daf5b9
+ZONE_ID=Z09974681JREIYBUXFTO2
 
 for INSTANCE in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --key-name devops --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" --query 'Instances[0].InstanceId' --output text)
 
-    echo "$INSTANCE_ID"
+    if [ INSTANCE != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch "{
+    \"Changes\": [{
+      \"Action\": \"UPSERT\",
+      \"ResourceRecordSet\": {
+        \"Name\": \"$INSTANCE.ayri.fun\",
+        \"Type\": \"A\",
+        \"TTL\": 1,
+        \"ResourceRecords\": [{
+          \"Value\": \"$IP\"
+        }]
+      }
+    }]
+  }"
 done
