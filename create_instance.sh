bd46ba5229f6401816be4c41ca6996dd8ce9c39f
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-07-15 23:28:29 +0530

    Practice

---
+++ create_instance.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+AMI_ID=ami-002192a70217ac181
+SG_ID=sg-014ee579326daf5b9
+
+for INSTANCE in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --key-name devops \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text)
+done
+
+echo "$INSTANCE: $INSTANCE_ID"
