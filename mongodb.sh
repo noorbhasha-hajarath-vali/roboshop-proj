a16cd0c42b9a55828791b66870816551124445a8
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-07-15 23:14:56 +0530

    Practice

--- mongodb.sh
+++ mongodb.sh
@@ -5,4 +5,9 @@ USER_ID=$(id -u)
 LOG_DIR=/var/log/shell-roboshop
 FILE_NAME=$(basename $0 .sh)
 LOG_FILE=$LOG_DIR/$FILE_NAME.log
-echo $LOG_FILE
+
+if [$USER_ID -ne 0 ]; then
+    echo "User doesn't have root privilages, Run with sudo"
+fi
+
+echo "Process Started at $(date)"
