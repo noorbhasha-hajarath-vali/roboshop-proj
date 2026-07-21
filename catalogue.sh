714b997ab1c63096bfb0d641bad4d29984966435
noorbhasha-hajarath-vali <hajarathvalinoor@gmail.com>
2026-07-22 00:59:44 +0530

    Create_instance file Updated

--- catalogue.sh
+++ catalogue.sh
@@ -30,3 +30,55 @@ VALIDATE() {
     fi
 }
 
+dnf module disable nodejs -y
+VALIDATE $? "disable nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "enable nodejs:20"
+
+dnf install nodejs -y
+VALIDATE $? "install nodejs"
+
+id roboshop
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "User Creation"
+else
+    echo "User Already Exists.. $Y SKIPPING $NC"
+fi
+
+mkdir /app
+VALIDATE $? "Create app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+VALIDATE $? "Download Catalogue code"
+
+cd /app
+VALIDATE $? "Go to app directory"
+
+unzip /tmp/catalogue.zip
+VALIDATE $? "unxip catalogue code"
+
+npm install
+VALIDATE $? "Install Depenencies"
+
+cp "$SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Install Depenencies"
+
+systemctl daemon-reload
+VALIDATE $? "Reload Deamon"
+
+systemctl enable catalogue
+VALIDATE $? "enable catalogue service"
+
+systemctl start catalogue
+VALIDATE $? "Start catalogue service"
+
+cp "$SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copy MongoDB Repo"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "Install MongoDB Client"
+
+mongosh --host mongodb.ayri.fun </app/db/master-data.js
+VALIDATE $? "Load Master Data"
